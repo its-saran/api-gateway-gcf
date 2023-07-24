48597c989117b018d3d0ce792577c1ef6de32307
its-saran <its.saran.pv@gmail.com>
2023-07-24 19:18:19 +0530

    Implement proxy middleware

--- api/middlewares/proxyRouter.js
+++ api/middlewares/proxyRouter.js
@@ -1,1 +1,27 @@
+import axios from 'axios';
+
+const proxyMiddleware = (req, res, next) => {
+
+    const target = `https://asia-south1-whrilapi.cloudfunctions.net/`
+    const service = req.log.gatewayReq.service
+    const serviceQuery = `${req.originalUrl}`.replace(`${req.log.gatewayReq.service}/`, '').replace(/&?apikey=[^&]*/g, '')
+    const logQuery = `&logId=${req.log.id}&logPath=${req.log.path}`
+    const finalUrl = target+service+serviceQuery+logQuery
+    console.log(`Final proxy url: ${finalUrl}`)
+
+    axios.get(finalUrl)
+      .then(response => {
+          console.log('Response sent')
+          res.send({
+            data: req.testData,
+            responseData: response.data
+        })
+      })
+      .catch(error => {
+          console.error('Error:', error.message);
+          res.send(error.message)
+      });
+};
+
+export default proxyMiddleware;
 
